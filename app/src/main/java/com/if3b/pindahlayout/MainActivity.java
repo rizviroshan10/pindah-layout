01810da9243c591c563c35452c18eaf77dc84f8e
rizvi roshan <rizviroshan10@mhs.mdp.ac.id>
2022-10-03 11:20:31 +0700

    pindah layout berhasil

--- app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnpindah;
+    EditText etnama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setTitle("layout A");
+
         btnpindah = findViewById(R.id.btn_pindah);
+        etnama = findViewById(R.id.et_nama);
+
         btnpindah.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View view) {
                 Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
