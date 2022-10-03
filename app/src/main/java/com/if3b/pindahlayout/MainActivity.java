f16f3382553521108f0d20da0fe1fd8aafdabb0a
rizvi roshan <rizviroshan10@mhs.mdp.ac.id>
2022-10-03 10:44:07 +0700

    percobaan inten pertama

--- app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -2,13 +2,29 @@ package com.if3b.pindahlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnpindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnpindah = findViewById(R.id.btn_pindah);
+        btnpindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(pindah);
+
+            }
+        });
+
     }
 }
